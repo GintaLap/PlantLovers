76c15f51fa390b3811f20a3a4690391c60a62a1f
GintaLap <69529030+GintaLap@users.noreply.github.com>
2021-03-24 22:18:22 +0200

    Added images, Plant class& User class

--- src/main/java/com/example/plant_lovers/data/Plant.java
+++ src/main/java/com/example/plant_lovers/data/Plant.java
@@ -1,4 +1,21 @@
 package com.example.plant_lovers.data;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Plant {
+    private int id;
+    private int scienceName;
+    private int name;
+    private String description;
+    private String sunlight;
+    private String moisture;
+    private int watering;
+    private boolean petToxic;
+    private String type;
+    private String humidity;
+
 }
--- src/main/java/com/example/plant_lovers/data/User.java
+++ src/main/java/com/example/plant_lovers/data/User.java
@@ -1,4 +1,19 @@
 package com.example.plant_lovers.data;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class User {
+    private int id;
+    private String login;
+    private String name;
+    private String password;
+    private int userPlantId;
+
+
+
+
 }
