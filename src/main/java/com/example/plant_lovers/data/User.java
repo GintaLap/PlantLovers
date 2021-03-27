6120ae9d7ff1b1980fa2936131cdf142e65a6269
arta-blu <75677671+arta-blu@users.noreply.github.com>
2021-03-27 21:13:16 +0200

    1st - user class updated

--- src/main/java/com/example/plant_lovers/data/User.java
+++ src/main/java/com/example/plant_lovers/data/User.java
@@ -3,17 +3,33 @@ package com.example.plant_lovers.data;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
-    private int id;
+    private Integer id;
     private String login;
     private String name;
     private String password;
-    private int userPlantId;
+    private Integer userPlantId;
 
+    @SneakyThrows
+    public static User create(ResultSet rs) {
 
+        var user = new User (rs.getInt("user_id"),
+                rs.getString("user_login"),
+                rs.getString("user_name"),
+                rs.getString("user_password"),
+                rs.getInt("user_plant_id"));
+
+        return user;
+    }
 
 
 }
