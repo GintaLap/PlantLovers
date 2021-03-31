aa80c8c2cf6c5d11a048bf52f83ca6238b786668
arta-blu <75677671+arta-blu@users.noreply.github.com>
2021-03-31 21:55:20 +0300

    Updated UserController and from html taken away link to My_Garden.

--- src/main/java/com/example/plant_lovers/controllers/UserController.java
+++ src/main/java/com/example/plant_lovers/controllers/UserController.java
@@ -7,6 +7,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class UserController {
 
@@ -25,44 +28,51 @@ public class UserController {
 
 
     @PostMapping("/login")
-    public String login(UserDTO dto, Model model) {
+    public String login(UserDTO udto, Model model, HttpServletRequest request) {
 
-        var user = dm.login(dto.getUEmail(), dto.getUPassword());
+        var user = dm.login(udto.getUEmail(), udto.getUPassword());
 
         if (user == null) {
             model.addAttribute("error", "Unable to login");
             model.addAttribute("hasError", true);
             return "login";
         }
-
+        request.getSession().setAttribute("User", user);
+        model.addAttribute("user", user);
         return "your_garden";
     }
 
+    @GetMapping("/your_garden")
+    public String getGarden(Model model, HttpSession session) {
+       var user = (User)session.getAttribute("User");
+        model.addAttribute("user", user);
+        return "your_garden";
+    }
 
     @PostMapping("/process_register")
     public String register(UserDTO dto, Model model) {
-
         var user = new User(0, dto.getUEmail(), dto.getULogin(), dto.getUName(), dto.getUPassword());
-
         dm.addUser(user);
-
         return "login";
     }
 
 
-    @GetMapping("/your_garden")
-    public String addGarden(Model model) {
+//    @GetMapping("/your_garden")
+//    public String addGarden(Model model) {
+//        var dmg = new DataManagerGarden();
+//        var garden = dmg.getGarden();
+//        model.addAttribute("addedGarden", garden);
+//        var user = dm.getUsers();
+//        model.addAttribute("user", user);
+//        dmg.addGarden(garden);
+//        return "your_garden";
+//    }
 
-        var dm = new DataManagerGarden();
-
-
-        return "your_garden";
-    }
 
     @GetMapping("/home")
-    public String back(Model model) {
-
-
+    public String getIndex(Model model) {
+        model.addAttribute("error","");
+        model.addAttribute("hasError", false);
         return "index";
     }
 }
